835cf07d4998111849a2d481279cd8afbbdd33be
Seth Cooper <seth.cooper@gmail.com>
2024-01-22 12:50:52 -0500

    Add Processing test files.

---
+++ processing/make_games.sh
@@ -0,0 +1,7 @@
+mkdir -p games
+python yaml2json.py ../games/ttt.yaml games/ttt-text
+python yaml2json.py ../games/connect4.yaml games/connect4-text
+python yaml2json.py ../games/checkers.yaml games/checkers --sprites ../games/sprites/checkers/checkers.yaml
+python yaml2json.py ../games/dungeon_patrol.yaml games/dungeon_patrol-text
+python yaml2json.py ../games/dungeon_patrol.yaml games/dungeon_patrol --sprites ../games/sprites/dungeon/dungeon.yaml
+python yaml2json.py ../games/dungeon_chase.yaml games/dungeon_chase --sprites ../games/sprites/dungeon/dungeon.yaml
