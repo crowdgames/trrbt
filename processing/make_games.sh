ef322b6e9fb541ca5afd12e7df566bdef98301ba
Seth Cooper <seth.cooper@gmail.com>
2024-02-05 09:46:31 -0500

    Sprites.

--- processing/make_games.sh
+++ processing/make_games.sh
@@ -7,4 +7,6 @@ python yaml2json.py ../games/checkers.yaml games/checkers --sprites ../games/spr
 python yaml2json.py ../games/dungeon_patrol.yaml games/dungeon_patrol-text
 python yaml2json.py ../games/dungeon_patrol.yaml games/dungeon_patrol --sprites ../games/sprites/dungeon/dungeon.yaml
 python yaml2json.py ../games/dungeon_chase.yaml games/dungeon_chase --sprites ../games/sprites/dungeon/dungeon.yaml
+python yaml2json.py ../games/soko.yaml games/soko --sprites ../games/sprites/soko/soko.yaml
 python yaml2json.py ../games/rust.yaml games/rust --sprites ../games/sprites/dungeon/rust.yaml
+python yaml2json.py ../games/rust-layer.yaml games/rust-layer --sprites ../games/sprites/dungeon/rust-layer.yaml
