ba8be6efcdbb4ecf714b4f4e4003d4fa3570e235
Seth Cooper <seth.cooper@gmail.com>
2024-02-09 08:46:08 -0500

    Remove duplicate choices; add blockdude sprites.

--- game_viz.py
+++ game_viz.py
@@ -1,6 +1,7 @@
 import argparse
 import copy
 import game
+import json
 import os
 import PIL.Image, PIL.ImageDraw, PIL.ImageTk
 import random
@@ -146,7 +147,7 @@ class GameFrame(tkinter.Frame):
                         colmid = col + cols / 2.0
                         dist_sqr = (mr - rowmid) ** 2 + (mc - colmid) ** 2
                         if choice is None or dist_sqr < best_choice:
-                            idx, desc, lhs, rhs = choices[max(0, min(len(choices) - 1, int(len(choices) * ((mc - col) / cols))))]
+                            idx, desc, rhs = choices[max(0, min(len(choices) - 1, int(len(choices) * ((mc - col) / cols))))]
                             choice = idx
                             best_choice = dist_sqr
 
@@ -250,18 +251,27 @@ class GameFrame(tkinter.Frame):
                     self._fg_cids[key] = (tiles, new_cids)
 
     def update_choices(self, player_id, choices, choice_board):
+        choices_unique = dict()
+        choices_seen = set()
+        for idx, choice in choices.items():
+            choicek = json.dumps(choice, sort_keys=True)
+            if choicek not in choices_seen:
+                choices_seen.add(choicek)
+                choices_unique[idx] = choice
+        choices = choices_unique
+
         self._choices_by_idx = {}
         self._choices_by_rect = {}
-        for idx, (desc, lhs, rhs, row, col) in choices.items():
-            rows, cols = util.layer_pattern_size(lhs)
+        for idx, (desc, rhs, row, col) in choices.items():
+            rows, cols = util.layer_pattern_size(rhs)
 
-            self._choices_by_idx[idx] = (desc, lhs, rhs, row, col)
+            self._choices_by_idx[idx] = (desc, rhs, row, col)
 
             rect = (row, col, rows, cols)
             if rect not in self._choices_by_rect:
                 self._choices_by_rect[rect] = []
 
-            self._choices_by_rect[rect].append((idx, desc, lhs, rhs))
+            self._choices_by_rect[rect].append((idx, desc, rhs))
 
         corner = self._cell_size / 4
         corner_box = corner * 1.5
@@ -284,7 +294,7 @@ class GameFrame(tkinter.Frame):
         self._choice_cids[None] = []
 
         for (row, col, rows, cols), choices in self._choices_by_rect.items():
-            for ii, (idx, desc, lhs, rhs) in enumerate(choices):
+            for ii, (idx, desc, rhs) in enumerate(choices):
                 self._choice_cids[idx] = []
 
                 for rr in range(rows):
@@ -381,7 +391,7 @@ class GameFrame(tkinter.Frame):
                     self._cvs.delete(choice_cid)
             self._choice_cids = {}
 
-            desc, lhs, rhs, row, col = self._choices_by_idx[choice]
+            desc, rhs, row, col = self._choices_by_idx[choice]
 
             tmp_board = copy.deepcopy(game_proc.board)
             lpatt = rhs
