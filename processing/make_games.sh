51a1649d10ec8f8d07cb0e676a1adda1e1908002
Seth Cooper <seth.cooper@gmail.com>
2024-02-12 08:30:40 -0500

    Update processing games.

--- processing/make_games.sh
+++ processing/make_games.sh
@@ -3,13 +3,12 @@ mkdir -p games
 
 python yaml2json.py ../games/ttt.yaml games/ttt-text
 python yaml2json.py ../games/connect4.yaml games/connect4-text
+python yaml2json.py ../games/checkers.yaml games/checkers-text
 python yaml2json.py ../games/checkers.yaml games/checkers --sprites ../games/sprites/checkers/checkers.yaml
+python yaml2json.py ../games/soko.yaml games/soko-text
+python yaml2json.py ../games/soko.yaml games/soko --sprites ../games/sprites/soko/soko.yaml
 python yaml2json.py ../games/blockdude.yaml games/blockdude-text
 python yaml2json.py ../games/blockdude.yaml games/blockdude --sprites ../games/sprites/blockdude/blockdude.yaml
-python yaml2json.py ../games/dungeon_patrol.yaml games/dungeon_patrol-text
-python yaml2json.py ../games/dungeon_patrol.yaml games/dungeon_patrol --sprites ../games/sprites/dungeon/dungeon.yaml
-python yaml2json.py ../games/dungeon_chase.yaml games/dungeon_chase --sprites ../games/sprites/dungeon/dungeon.yaml
-python yaml2json.py ../games/soko.yaml games/soko --sprites ../games/sprites/soko/soko.yaml
 python yaml2json.py ../games/rust.yaml games/rust-text
 python yaml2json.py ../games/rust.yaml games/rust --sprites ../games/sprites/dungeon/rust.yaml
 python yaml2json.py ../games/turn.yaml games/turn-text
