83e94a60a112d80f5d9975589ec97b20252b600d
Seth Cooper <seth.cooper@gmail.com>
2024-02-11 20:37:08 -0500

    Add turn sprites.

--- processing/make_games.sh
+++ processing/make_games.sh
@@ -10,5 +10,7 @@ python yaml2json.py ../games/dungeon_patrol.yaml games/dungeon_patrol-text
 python yaml2json.py ../games/dungeon_patrol.yaml games/dungeon_patrol --sprites ../games/sprites/dungeon/dungeon.yaml
 python yaml2json.py ../games/dungeon_chase.yaml games/dungeon_chase --sprites ../games/sprites/dungeon/dungeon.yaml
 python yaml2json.py ../games/soko.yaml games/soko --sprites ../games/sprites/soko/soko.yaml
+python yaml2json.py ../games/rust.yaml games/rust-text
 python yaml2json.py ../games/rust.yaml games/rust --sprites ../games/sprites/dungeon/rust.yaml
-python yaml2json.py ../games/turn.yaml games/turn
+python yaml2json.py ../games/turn.yaml games/turn-text
+python yaml2json.py ../games/turn.yaml games/turn --sprites ../games/sprites/turn/turn.yaml
