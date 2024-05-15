21743f66171ab132c1f8a60be704f818f61e0c0d
Kaylah Facey <kaylah.facey@gmail.com>
2024-05-15 15:19:13 -0400

    add limerick and if games to make_games

--- processing/make_games.sh
+++ processing/make_games.sh
@@ -19,6 +19,10 @@ python yaml2json.py ../games/soko.yaml out/games/soko-text
 python yaml2json.py ../games/soko.yaml out/games/soko --sprites ../games/sprites/soko/soko.yaml
 python yaml2json.py ../games/blockdude.yaml out/games/blockdude-text
 python yaml2json.py ../games/blockdude.yaml out/games/blockdude --sprites ../games/sprites/blockdude/blockdude.yaml
+python yaml2json.py ../games/limerick.yaml out/games/limerick-text
+python yaml2json.py ../games/limerick.yaml out/games/limerick --sprites ../games/sprites/limerick/limerick.yaml
+python yaml2json.py ../games/choice.yaml out/games/cyoa
+python yaml2json.py ../games/lost_and_found.yaml out/games/text-adventure
 python yaml2json.py ../games/rust.yaml out/games/rust-text
 python yaml2json.py ../games/rust.yaml out/games/rust --sprites ../games/sprites/dungeon/rust.yaml
 python yaml2json.py ../games/turn.yaml out/games/turn-text
