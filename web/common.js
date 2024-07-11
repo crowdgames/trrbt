1b0f1cec406f6845b1c736bbf67abd4e0e336f65
Seth Cooper <seth.cooper@gmail.com>
2024-07-11 10:10:11 -0400

    Prefix names in different files.

---
+++ web/common.js
@@ -0,0 +1,24 @@
+const BUTTON_LEFT = 0;
+const BUTTON_RIGHT = 2;
+const PIXEL_RATIO = window.devicePixelRatio;
+const DOUBLE_CLICK_TIME = 300;
+
+function copymap(map) {
+    if (map === null) {
+        return null;
+    } else {
+        return new Map(JSON.parse(JSON.stringify(Array.from(map))));
+    }
+}
+
+function deepcopyobj(obj) {
+    if (obj === null) {
+        return null;
+    } else if (obj === true) {
+        return true;
+    } else if (obj === false) {
+        return false;
+    } else {
+        return JSON.parse(JSON.stringify(obj));
+    }
+}
