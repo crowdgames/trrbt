e34460e9b65f888c2193773958344a3af35e2b9e
Seth Cooper <seth.cooper@gmail.com>
2025-08-09 20:28:18 -0400

    Account for empty output.

--- inversion_tools/invert_util.py
+++ inversion_tools/invert_util.py
@@ -8,4 +8,4 @@ def script_output_json(*script_and_args):
     return json.loads(script_output(*script_and_args))
 
 def script_output_jsons(*script_and_args):
-    return [json.loads(line) for line in script_output(*script_and_args).strip().split('\n')]
+    return [json.loads(line) for line in script_output(*script_and_args).strip().split('\n') if line != '']
